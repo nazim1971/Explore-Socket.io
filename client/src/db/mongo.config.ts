7f43ddd7b87bea941ddeaab4c9d31496f73d73bc
nazim1971 <md.nazimuddinaj@gmail.com>
2024-09-28 15:31:32 +0600

    connect mongodb in nextApp

---
+++ client/src/db/mongo.config.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+const uri  = process.env.MY_DB;
+
+if (!uri) {
+    throw new Error("MongoDB URI is not defined in the environment variables");
+  }
+
+async function connectDB() {
+    try {
+        if (mongoose.connection.readyState >= 1) {
+            // If already connected, no need to connect again
+            return;
+          }
+      await mongoose.connect(uri!); 
+      console.log("Connected to MongoDB successfully!");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      process.exit(1); 
+    }
+  }
+  
+export default connectDB;
